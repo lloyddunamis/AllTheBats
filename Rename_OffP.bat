1216ffaeff92b0a2996785748d8d7ce431ee3991
Lloyd Dunamis <6012867+lloyddunamis@users.noreply.github.com>
2025-03-17 07:20:17 +0800

    Create Rename_OffP.bat

---
+++ Rename_OffP.bat
@@ -0,0 +1,13 @@
+ren SAOFB-WindowsNoEditor_0_P.pak SAOFB-WindowsNoEditor_0.pak
+ren SAOFB-WindowsNoEditor_0_P.sig SAOFB-WindowsNoEditor_0.sig
+ren SAOFB-WindowsNoEditor_1_P.pak SAOFB-WindowsNoEditor_1.pak
+ren SAOFB-WindowsNoEditor_1_P.sig SAOFB-WindowsNoEditor_1.sig
+ren SAOFB-WindowsNoEditor_2_P.pak SAOFB-WindowsNoEditor_2.pak
+ren SAOFB-WindowsNoEditor_2_P.sig SAOFB-WindowsNoEditor_2.sig
+ren SAOFB-WindowsNoEditor_3_P.pak SAOFB-WindowsNoEditor_3.pak
+ren SAOFB-WindowsNoEditor_3_P.sig SAOFB-WindowsNoEditor_3.sig
+ren SAOFB-WindowsNoEditor_4_P.pak SAOFB-WindowsNoEditor_4.pak
+ren SAOFB-WindowsNoEditor_4_P.sig SAOFB-WindowsNoEditor_4.sig
+ren SAOFB-WindowsNoEditor_UI_P.pak SAOFB-WindowsNoEditor_UI.pak
+ren SAOFB-WindowsNoEditor_UI_P.sig SAOFB-WindowsNoEditor_UI.sig
+pause
